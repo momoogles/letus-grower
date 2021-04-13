7efff075261bc96f0c82b3e7dd18aa3eb50dd583
momoogles <mo1419091@gmail.com>
2021-04-13 22:46:59 +0900

    storybookの背景の色をLETUSに合わせた

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,10 @@ import { VIEWPORTS } from '../utils/viewports'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'default',
+    values: [{ name: 'default', value: '#f8f9fa' }],
+  },
   viewport: {
     viewports: {
       ...VIEWPORTS,
