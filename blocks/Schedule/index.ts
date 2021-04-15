b4655d5c916cfc569c15c2e2492bacfc49eeba20
momoogles <mo1419091@gmail.com>
2021-04-15 10:06:49 +0900

    コンテンツの記述を簡潔にした

--- blocks/Schedule/index.ts
+++ blocks/Schedule/index.ts
@@ -4,24 +4,24 @@ import './style.css'
 type Week = '月' | '火' | '水' | '木' | '金'
 type ContentName = string
 type ContentType = 'hishusentaku' | 'hishu' | 'sentaku' | 'ippan'
-type Content = [ContentName, ContentType]
+type Content = [ContentName, ContentType?]
 
 const CONTENTS: {
   [week in Week]: Content[]
 } = {
-  月: [],
-  火: [],
-  水: [],
-  木: [],
-  金: [],
+  月: [[''], [''], [''], [''], ['']],
+  火: [[''], [''], [''], [''], ['']],
+  水: [[''], [''], [''], [''], ['']],
+  木: [[''], [''], [''], [''], ['']],
+  金: [[''], [''], [''], [''], ['']],
 }
 
 const TIMEINTERVALS = [
   '08:50 - 10:20',
-  '10:00 - 12:00',
-  '12:00 - 14:00',
-  '14:00 - 16:00',
-  '16:00 - 18:00',
+  '10:30 - 12:00',
+  '12:50 - 14:20',
+  '14:30 - 16:00',
+  '16:10 - 17:40',
 ]
 
 const template = html`
@@ -39,37 +39,11 @@ const template = html`
         )}
       </div>
       <div class="content-list">
-        <!-- 月 -->
-        <a class="content-item"></a>
-        <a class="content-item"></a>
-        <a class="content-item"></a>
-        <a class="content-item"></a>
-        <!-- 火 -->
-        <a class="content-item"></a>
-        <a class="content-item"></a>
-        <a class="content-item"></a>
-        <a class="content-item"></a>
-        <a class="content-item"></a>
-        <!-- 水 -->
-        <a class="content-item hishusentaku" href="#">ほげほげほ論1 </a>
-        <a class="content-item hishusentaku" href="#">ふがふが論1</a>
-        <a class="content-item"></a>
-        <a class="content-item hishu" href="#">ほげほげ研究1</a>
-        <a class="content-item hishu" href="#">ほげほげ研究1</a>
-        <!-- 木 -->
-        <a class="content-item"></a>
-        <a class="content-item sentaku" href="#">ほげほげほ1</a>
-        <a class="content-item sentaku" href="#">ふがふがふ1</a>
-        <a class="content-item ippan" href="#">ほげほげ及びふがふが1</a>
-        <a class="content-item"></a>
-        <!-- 金 -->
-        <a class="content-item hishusentaku" href="#">ほげほげ学3</a>
-        <a class="content-item hishusentaku" href="#">ふがふが論1</a>
-        <a class="content-item ippan" href="#">ほげ学1</a>
-        <a class="content-item sentaku" href="#"
-          >ほげほげほげほげほげリサーチ1</a
-        >
-        <a class="content-item"></a>
+        ${Object.values(CONTENTS).flatMap(
+          (content) => html`
+            <a class="content-item${` ${content[1]}`}">${content[0]}</a>
+          `
+        )}
       </div>
     </div>
   </div>
