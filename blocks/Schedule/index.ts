a108bb537a2fef2c5ff8d287e4b008f797ddcbfb
momoogles <mo1419091@gmail.com>
2021-04-15 09:45:43 +0900

    変数を用意した

--- blocks/Schedule/index.ts
+++ blocks/Schedule/index.ts
@@ -1,29 +1,45 @@
 import { html } from 'lit-html'
 import './style.css'
 
+type Week = '月' | '火' | '水' | '木' | '金'
+type ContentName = string
+type ContentType = 'hishusentaku' | 'hishu' | 'sentaku' | 'ippan'
+type Content = [ContentName, ContentType]
+
+const CONTENTS: {
+  [week in Week]: Content[]
+} = {
+  月: [],
+  火: [],
+  水: [],
+  木: [],
+  金: [],
+}
+
+const TIMEINTERVALS = [
+  '08:50 - 10:20',
+  '10:00 - 12:00',
+  '12:00 - 14:00',
+  '14:00 - 16:00',
+  '16:00 - 18:00',
+]
+
 const template = html`
   <div class="schedule-wrapper">
     <div class="schedule">
       <div class="week-list">
-        ${['月', '火', '水', '木', '金'].map(
+        ${Object.keys(CONTENTS).map(
           (week) => html`<div class="week-item">${week}</div>`
         )}
       </div>
       <div class="timeinterval-list">
-        ${[
-          '08:50 - 10:20',
-          '10:00 - 12:00',
-          '12:00 - 14:00',
-          '14:00 - 16:00',
-          '16:00 - 18:00',
-        ].map(
+        ${TIMEINTERVALS.map(
           (timeInterval) =>
             html`<div class="timeinterval-item">${timeInterval}</div>`
         )}
       </div>
       <div class="content-list">
         <!-- 月 -->
-        <a class="content-item"></a>
         <a class="content-item"></a>
         <a class="content-item"></a>
         <a class="content-item"></a>
